9703606b8d62535409b449e82b506d2375742c8d
Sebastian-Stepper <Sebastian.Stepper@mr-pixel.de>
2024-09-01 22:40:37 +0200

    stats cashing

--- src/services/statsService.js
+++ src/services/statsService.js
@@ -1,47 +1,79 @@
-
+const NodeCache = require('node-cache');
+const myCache = new NodeCache({ stdTTL: 10 });
 
 const User = require('../models/userModel');
 const Character = require('../models/characterModel');
 const Anime = require('../models/animeModel');
-const requestLog = require("../models/requestLogModel");
+const requestLog = require('../models/requestLogModel');
+
+const CACHE_KEYS = {
+    USER_COUNT: 'user_count',
+    DATABASE_ENTRIES: 'database_entries',
+    REQUESTS_LAST_30_DAYS: 'requests_last_30_days'
+};
 
 exports.getTotalRequestsLast30Days = async () => {
-    try {
-        const thirtyDaysAgo = new Date();
-        thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+    const cacheKey = CACHE_KEYS.REQUESTS_LAST_30_DAYS;
+    let count = myCache.get(cacheKey);
 
-        return await requestLog.countDocuments({
-            timestamp: {$gte: thirtyDaysAgo}
-        });
-    } catch (err) {
-        console.error('Error fetching request count:', err);
-        throw err;
+    if (count === undefined) {
+        try {
+            const thirtyDaysAgo = new Date();
+            thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+
+            count = await requestLog.countDocuments({
+                timestamp: { $gte: thirtyDaysAgo }
+            });
+
+            myCache.set(cacheKey, count);
+        } catch (err) {
+            console.error('Error fetching request count:', err);
+            throw err;
+        }
     }
+
+    return count;
 };
-exports.getDatabaseEntries = async () => {
-    try {
-        const [characterCount, animeCount] = await Promise.all([
-            Character.countDocuments({}),
-            Anime.countDocuments({})
-        ]);
 
-        return {
-            character: characterCount,
-            anime: animeCount
-        };
-    } catch (err) {
-        console.error('Error fetching database entries:', err);
-        throw err;
+exports.getDatabaseEntries = async () => {
+    const cacheKey = CACHE_KEYS.DATABASE_ENTRIES;
+    let entries = myCache.get(cacheKey);
+
+    if (entries === undefined) {
+        try {
+            const [characterCount, animeCount] = await Promise.all([
+                Character.countDocuments({}),
+                Anime.countDocuments({})
+            ]);
+
+            entries = {
+                character: characterCount,
+                anime: animeCount
+            };
+
+            myCache.set(cacheKey, entries);
+        } catch (err) {
+            console.error('Error fetching database entries:', err);
+            throw err;
+        }
     }
+
+    return entries;
 };
 
 exports.getUserCount = async () => {
-    try {
-        return await User.countDocuments({});
-    } catch (err) {
-        console.error('Error fetching user count:', err);
-        throw err;
+    const cacheKey = CACHE_KEYS.USER_COUNT;
+    let count = myCache.get(cacheKey);
+
+    if (count === undefined) {
+        try {
+            count = await User.countDocuments({});
+            myCache.set(cacheKey, count);
+        } catch (err) {
+            console.error('Error fetching user count:', err);
+            throw err;
+        }
     }
+
+    return count;
 };
-
-
