57546bfc999b3c489ba7f0e2b4727ab084e0cac1
Sebastian-Stepper <Sebastian.Stepper@mr-pixel.de>
2024-09-01 21:16:24 +0200

    more stats

--- src/controllers/statsController.js
+++ src/controllers/statsController.js
@@ -3,9 +3,14 @@ const statsService = require('../services/statsService');
 exports.getStats = async (req, res) => {
     try {
         const userCount = await statsService.getUserCount();
+        const DatabaseEntries = await statsService.getDatabaseEntries();
 
         res.json({
-            userCount
+            userCount,
+            AnimeEntries: DatabaseEntries.anime,
+            CharacterEntries: DatabaseEntries.character,
+
+
         });
     } catch (err) {
         res.status(500).json({ error: 'Failed to get statistics' });
--- src/services/statsService.js
+++ src/services/statsService.js
@@ -1,5 +1,26 @@
 
+
 const User = require('../models/userModel');
+const Character = require('../models/characterModel');
+const Anime = require('../models/animeModel');
+
+
+exports.getDatabaseEntries = async () => {
+    try {
+        const [characterCount, animeCount] = await Promise.all([
+            Character.countDocuments({}),
+            Anime.countDocuments({})
+        ]);
+
+        return {
+            character: characterCount,
+            anime: animeCount
+        };
+    } catch (err) {
+        console.error('Error fetching database entries:', err);
+        throw err;
+    }
+};
 
 exports.getUserCount = async () => {
     try {
