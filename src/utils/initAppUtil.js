3818480828cdbb147220f6723641f63ce4e26fb7
AIO <69240351+AIO-Develope@users.noreply.github.com>
2024-11-02 16:50:04 +0100

    Update initAppUtil.js

--- src/utils/initAppUtil.js
+++ src/utils/initAppUtil.js
@@ -10,12 +10,13 @@ const initApp = () => {
     const app = express();
     app.use(cors);
     app.use(express.json());
-    
+
     app.use('/assets', express.static(path.join(__dirname, '../assets')));
     loadRoute(app, '/auth', authRoutes);
     loadRoute(app, '/anime', animeRoutes);
     loadRoute(app, '/media', mediaRoutes);
-    
+    app.get('/', (req, res) => res.sendStatus(200));
+
     return app;
 };
 
