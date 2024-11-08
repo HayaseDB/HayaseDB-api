a9e4f619813266d17dbeb4a18ca32e138a6d3e17
AIO <69240351+AIO-Develope@users.noreply.github.com>
2024-11-08 22:44:06 +0100

    pg admin

--- src/utils/serverUtil.js
+++ src/utils/serverUtil.js
@@ -4,7 +4,7 @@ const logger = require('./loggerUtil')
 const { connectDB } = require('../config/databaseConfig');
 const figlet = require('figlet');
 const { createGradient } = require('./colorUtil');
-const { getUrl } = require('./urlUtil');
+const { getUrl } = require('./loaders/urlUtil');
 const PORT = process.env.PORT || 3000;
 
 const startServer = async (app) => {
@@ -24,13 +24,20 @@ const startServer = async (app) => {
         const webInfo = await getUrl(process.env.WEB_URL, `http://localhost:8080`);
         const docsInfo = await getUrl(`${process.env.API_URL}/docs`, `http://localhost:${PORT}/docs`);
         const dbInfo = await getUrl(`postgres://${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`, `no connection`);
+        let pgAdminInfo
+        if (process.env.NODE_ENV === 'development') {
+            pgAdminInfo = await getUrl('http://host.docker.internal:5050', `http://localhost:5050`);
+        }
 
         console.log(chalk.cyan('\n------------------------------------------------------'));
 
-        console.log(chalk[apiInfo.color](`    API:   ${apiInfo.url}`));
-        console.log(chalk[dbInfo.color](`    DB:    ${dbInfo.url}`));
-        console.log(chalk[docsInfo.color](`    DOCS:  ${docsInfo.url}`));
-        console.log(chalk[webInfo.color](`    WEB:   ${webInfo.url}`));
+        console.log(chalk[apiInfo.color](`    API:       ${apiInfo.url}`));
+        console.log(chalk[dbInfo.color](`    DB:        ${dbInfo.url}`));
+        console.log(chalk[docsInfo.color](`    DOCS:      ${docsInfo.url}`));
+        console.log(chalk[webInfo.color](`    WEB:       ${webInfo.url}`));
+        if (process.env.NODE_ENV === 'development') {
+            console.log(chalk[pgAdminInfo.color](`    PG Admin:  ${pgAdminInfo.url}`));
+        }
 
         console.log(chalk.cyan('------------------------------------------------------\n'));
     });
