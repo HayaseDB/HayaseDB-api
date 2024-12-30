7d3f4d9418b11a54b9f4d91bfcb07c03a3a541ad
AIO <69240351+AIO-Develope@users.noreply.github.com>
2024-12-30 14:24:05 +0100

    Update authMiddleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -66,8 +66,9 @@ const checkRateLimit = async (identifier, isApiKey = false) => {
 // todo - check if this is enough and if it can be spoofed
 // todo - Make website ssr requests internal
 const isRequestInternal = (req) => {
-    const forwardedFor = req.headers['cf-connecting-ip'];
-    return !forwardedFor;
+    const clientIp = req.headers['cf-connecting-ip'];
+    console.log(clientIp);
+    return !clientIp;
 };
 
 
