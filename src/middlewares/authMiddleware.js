ff649976a15ed824aa549b0409ca05c34187e098
AIO <69240351+AIO-Develope@users.noreply.github.com>
2024-12-30 15:38:14 +0100

    Update authMiddleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -67,12 +67,12 @@ const checkRateLimit = async (identifier, isApiKey = false) => {
 // todo - check if this is enough and if it can be spoofed
 // todo - Make website ssr requests internal
 const isRequestInternal = (req) => {
-    if ( req.headers['x-from-proxy']){
+    console.log(req.headers)
+    if (req.headers['x-from-proxy']){
         return true
     } else {
         return false
     }
-
 };
 
 
