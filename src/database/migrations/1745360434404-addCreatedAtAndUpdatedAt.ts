3dcf11d3b93c2a9fc43f0ec5477a86895aee7e01
Sebastian-Stepper <Sebastian.Stepper@mr-pixel.de>
2025-04-23 00:22:41 +0200

    Update 1745360434404-addCreatedAtAndUpdatedAt.ts

--- src/database/migrations/1745360434404-addCreatedAtAndUpdatedAt.ts
+++ src/database/migrations/1745360434404-addCreatedAtAndUpdatedAt.ts
@@ -4,13 +4,6 @@ export class AddCreatedAtAndUpdatedAt1745360434404 implements MigrationInterface
     name = 'AddCreatedAtAndUpdatedAt1745360434404'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        // Drop columns if they exist
-        await queryRunner.query(`ALTER TABLE "animes" DROP COLUMN IF EXISTS "createdAt"`);
-        await queryRunner.query(`ALTER TABLE "animes" DROP COLUMN IF EXISTS "updatedAt"`);
-        await queryRunner.query(`ALTER TABLE "contributions" DROP COLUMN IF EXISTS "dataCreatedat"`);
-        await queryRunner.query(`ALTER TABLE "contributions" DROP COLUMN IF EXISTS "dataUpdatedat"`);
-
-        // Re-add columns
         await queryRunner.query(`ALTER TABLE "animes" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`);
         await queryRunner.query(`ALTER TABLE "animes" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT now()`);
         await queryRunner.query(`ALTER TABLE "contributions" ADD "dataCreatedat" TIMESTAMP NOT NULL DEFAULT now()`);
