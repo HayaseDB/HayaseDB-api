3b1cad1b3ddcf3b46ded0695672a154590e31dd9
Sebastian-Stepper <Sebastian.Stepper@mr-pixel.de>
2025-04-23 02:52:26 +0200

    Update 1745368711348-nullableTitle.ts

--- src/database/migrations/1745368711348-nullableTitle.ts
+++ src/database/migrations/1745368711348-nullableTitle.ts
@@ -5,23 +5,14 @@ export class NullableTitle1745368711348 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "animes" ALTER COLUMN "title" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "animes" ALTER COLUMN "title" SET DEFAULT NULL`);
-
         await queryRunner.query(`ALTER TABLE "contributions" ALTER COLUMN "dataTitle" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "contributions" ALTER COLUMN "dataTitle" SET DEFAULT NULL`);
-
         await queryRunner.query(`ALTER TABLE "contributions" ALTER COLUMN "originalAnimeTitle" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "contributions" ALTER COLUMN "originalAnimeTitle" SET DEFAULT NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "contributions" ALTER COLUMN "originalAnimeTitle" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "contributions" ALTER COLUMN "originalAnimeTitle" DROP DEFAULT`);
-
         await queryRunner.query(`ALTER TABLE "contributions" ALTER COLUMN "dataTitle" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "contributions" ALTER COLUMN "dataTitle" DROP DEFAULT`);
-
         await queryRunner.query(`ALTER TABLE "animes" ALTER COLUMN "title" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "animes" ALTER COLUMN "title" DROP DEFAULT`);
     }
+
 }
